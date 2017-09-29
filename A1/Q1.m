a9bc77f6cd43377b78cf1f8c09ad3ef0fcaf030b
chaowangcanada <chaowang.ubc@gmail.com>
2017-09-28 23:49:31 -0400

    a1 modification

--- A1/Q1.m
+++ A1/Q1.m
@@ -14,7 +14,7 @@ extend = ones(size(x));
 
 x_extend = [x extend];
 
-W = LinearRegression(x_extend, y)
+W = LinearRegression(x_extend, y);
 
 W_x = (min(x):0.1:max(x))';
 W_y = [W_x, ones(size(W_x))]*W;
@@ -22,7 +22,7 @@ W_y = [W_x, ones(size(W_x))]*W;
 plot(W_x, W_y,'-r'); 
 
 %Q 1.c, find error function
-error =  ((x_extend*W - y).^2)/2;
+error = sum(((x_extend*W - y).^2))/2;
 disp('Linear regression error function'), disp(error);
 
 
@@ -37,20 +37,23 @@ function W = PolyRegress(X,Y,d,norm='no')
 end
 
 %Q1.e quadratic fit, and error function
-W = PolyRegress(x, y, 2)
+W = PolyRegress(x, y, 2);
 PlotPoly(x,y,W,2, '-g');
 x_expand = ExpandX(x,2);
 error = sum((x_expand*W - y).^2)/2;
 disp('quadratic regression error function'), disp(error);
 
 %Q1.f cubic fit, and error function
-W = PolyRegress(x, y, 3)
+W = PolyRegress(x, y, 3);
 PlotPoly(x,y,W,3, '-m');
 x_expand = ExpandX(x,3);
 error = sum((x_expand*W - y).^2)/2;
 disp('cubic regression error function'), disp(error);
 
 hold off
+title('linear,quadratic,cubic regression fit to data model')
+xlabel('x')
+ylabel('y')
 legend('scatter','linear', 'quadratic', 'cubic','Location','southeast')
 
 %Q1.g explaination on report
@@ -59,27 +62,35 @@ legend('scatter','linear', 'quadratic', 'cubic','Location','southeast')
 kFoldValidation(x, y, 10, 5);
 
 W = PolyRegress(x, y, 6, 'no');
-PlotPoly(x,y,W,6, '-m');
 x_expand = ExpandX(x,6);
 error = sum((x_expand*W - y).^2)/2;
-
 disp('six deg poly regression error function'), disp(error);
-
-hold off
-legend('scatter','six deg','Location','southeast')
-
-%legend('scatter','linear', 'quadratic', 'cubic','Location','southeast')
+%plot best fit polinomial regression
+figure
+scatter(x,y)
+hold on
+PlotPoly(x,y,W,6, '-m')
+title('best fit 6 degree polinomial regression')
+xlabel('x')
+ylabel('y')
 
 %Q1.i normalized five-fold cross validation
 disp('normalized: ');
 kFoldValidation(x, y, 10, 5, 'yes');
 
 W = PolyRegress(x, y, 6, 'yes');
-PlotPoly(x,y,W,6, '-m');
 x_expand = ExpandX(x,6);
 x_expand = Normalize(x_expand);
 error = sum((x_expand*W - y).^2)/2;
 disp('six deg normalized poly regression error function'), disp(error);
+%plot normalized best fit 
+figure
+scatter(Normalize(x),y)
+hold on
+PlotPoly(x_expand,y,W,6, '-m')
+title('best fit normalized 6 degree polinomial regression')
+xlabel('x')
+ylabel('y')
 
 function kFoldValidation(X,Y,d,k, norm = 'no')
   if mod(size(X)(1),k) == 0
@@ -110,6 +121,13 @@ function kFoldValidation(X,Y,d,k, norm = 'no')
       crossValidResult(degree, :) = [degree mean(error_train) mean(error_valid)];
     end 
     disp(sprintf('Cross Validation %d',k)),disp(crossValidResult);
+    %plot the error
+    figure
+    plot(crossValidResult(:,1), crossValidResult(:,2),'b-o',crossValidResult(:,1), crossValidResult(:,3),'r-s')
+    title('training error, validation error figure')
+    xlabel('degree')
+    ylabel('error')
+    legend('training error','validation error')
   end  
 end 
 
@@ -135,7 +153,7 @@ function F = ExpandX(X,d)
 end
 
 function PlotPoly(X, Y, W, d, font)
-  gridX = (min(X):0.1:max(X))';
+  gridX = (min(X):0.05:max(X))';
   gridY = ExpandX(gridX, d);
   plot(gridX, gridY*W, font);
 end
